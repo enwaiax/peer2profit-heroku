142b700fd8bf2e084ae59bd1905a5f26a17ebb42
Chasing66 <chasing66@live.com>
2021-08-31 15:49:22 +0000

    update app

--- webapp/app.py
+++ webapp/app.py
@@ -9,10 +9,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    hostname = socket.gethostname()
     ip = get('https://api.ipify.org').text
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"test.log")) as f:
         message = f.readlines()
-    return f"Hello from {ip}!"  + '<br>' + '<br>'.join(message)
+    return f"Hello from: {ip} on: {hostname}."  + '<br>' + '<p><a href="https://peer2profit.com/r/1629477772611fdb8cab06c" rel="noopener noreferrer" target="_blank">Check it</a></p>' + '<br>'.join(message)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- webapp/entrypoint.sh
+++ webapp/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 nohup /root/webapp/p2pclient -l $email >> /root/webapp/test.log 2>&1 &
+# check wheterh port is null
+if [ -z "$PORT" ]; then
+    PORT=5000
+fi
 gunicorn --bind 0.0.0.0:${PORT} wsgi 
