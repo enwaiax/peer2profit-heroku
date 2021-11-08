f884bf2713e32d2ea9c6acaabb942c7dfbefdac9
LuckyHunter <chasing66@live.com>
2021-11-08 21:15:42 +0800

    Update version to 0.56

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,13 @@
 FROM frolvlad/alpine-glibc
-ENV email=chasing0806@gmail.com
-
-# Add http server to serve the test.log
-RUN apk add --no-cache --update python3 py3-pip bash
-ADD ./webapp/requirements.txt /tmp/requirements.txt
-RUN pip3 install --no-cache-dir -q -r /tmp/requirements.txt
-# Add our code
-ADD ./webapp /root/webapp/
+RUN apk update && apk add --no-cache --update python3 py3-pip
 WORKDIR /root/webapp
-# Expose is NOT supported by Heroku
-# EXPOSE 5000 
+ADD ./webapp /root/webapp/
+RUN pip3 install --no-cache-dir -q -r /root/webapp/requirements.txt
 
+VOLUME ["/root/.config/"]
+ENV email=chasing66@live.com
+# Expose is NOT supported by Heroku
+# EXPOSE 5000
 # Run the app.  CMD is required to run on Heroku
 # $PORT is set by Heroku
 ENTRYPOINT ["sh", "-c", "/root/webapp/entrypoint.sh"]
--- webapp/app.py
+++ webapp/app.py
@@ -7,15 +7,15 @@ from requests import get
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     hostname = socket.gethostname()
     ip = get('https://api.ipify.org').text
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"test.log")) as f:
-        message = f.readlines()
-    return f"Hello from: {ip} on: {hostname}."  + '<br>' + '<p><a href="https://peer2profit.com/r/1629477772611fdb8cab06c" rel="noopener noreferrer" target="_blank">Check it</a></p>' + '<br>'.join(message)
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "test.log")) as f:
+        message = f.readlines()[-20:]
+    return f"Hello from: {ip} on: {hostname}." + '<br>' + '<p><a href="https://peer2profit.com/r/1629477772611fdb8cab06c" rel="noopener noreferrer" target="_blank">Check it</a></p>' + '<br>'.join(message)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
-
